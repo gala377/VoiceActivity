fac4ee8066563310bacee01c40bd8161400b81ab
Rafał Galczak <r.galczak@gmail.com>
2020-11-07 23:01:03 +0100

    Added simple code configured logging based on logging module

--- voice_activity/__main__.py
+++ voice_activity/__main__.py
@@ -1,14 +1,18 @@
 import dotenv
 import os
+import logging
 
 from voice_activity import bot
+
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
 
 def main():
     dotenv.load_dotenv()
     token = os.getenv("BOT_TOKEN")
     if token is None:
         raise RuntimeError("BOT_TOKEN env variable has to be set")
-    print("Running bot...")
+    LOGGER.info("Running bot...")
     bot.run_client(token)
 
 if __name__ == '__main__':
--- voice_activity/bot.py
+++ voice_activity/bot.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import re
+import logging
 
 from voice_activity import commands
 from collections import defaultdict
@@ -10,6 +11,9 @@ PER_CHAN_TIMEOUT = 60 # in seconds
 USER_CTX_TIMEOUT = 180 # in seconds
 
 COMMAND_REGEX = re.compile(r'([^"].*?|".*?")(?:\s|$)')
+
+LOGGER = logging.getLogger(__name__)
+
 
 class VoiceActivity(discord.Client):
 
@@ -23,17 +27,24 @@ class VoiceActivity(discord.Client):
         }
 
     async def on_message(self, message):
+        LOGGER.debug("got new message: '%s' from '%s'", message.content, message.author.name)
         if message.author == self.user:
+            LOGGER.debug("got self message")
             return
         elif message.guild is not None and self.user in message.mentions:
+            LOGGER.debug("called from guild by %s", message.author.name)
             await self._in_chan_callout(message)
         elif message.guild is None:
+            LOGGER.debug("got priv message: '%s' from '%s'", message.content, message.author.name)
             ctx = self._users_context.get(message.author.id)
             if ctx is None:
+                LOGGER.debug("no context found for user %s", message.author.name)
                 await message.author.send(
                     "sorry, could you call me from the server you want to execute commands in.")
             else:
+                LOGGER.debug("got context for user %s. Running command.", message.author.name)
                 await self._run_cmd(message.author, ctx.guild, message.content)
+        LOGGER.debug("message not importat for me")
 
     async def _in_chan_callout(self, message):
         reply = message.author
@@ -45,10 +56,13 @@ class VoiceActivity(discord.Client):
     async def _run_cmd(self, user, guild, content):
         try:
             cmd, args = self._parse_cmd(content)
+            LOGGER.info("got command %s from user %s", cmd, user.name)
             cmd = self._cmds.get(cmd, self._unknown_command)
             ctx = {"user": user, "guild": guild}
+            LOGGER.info("invoking command %s for user %s", cmd, user.name)
             await cmd(ctx, *args)
         except Exception as e:
+            LOGGER.info("couldn't parse command %a", e)
             await user.send(str(e))
 
     def _parse_cmd(self, mess):
@@ -67,27 +81,36 @@ class VoiceActivity(discord.Client):
         return cmd, args
 
     async def _unknown_command(self, ctx, *args):
+        LOGGER.info("got unknown command from user %s", ctx['user'].name)
         await ctx["user"].send("Unknown command, maybe try `help`?")
 
     async def on_voice_state_update(self, mem, bef, after):
         channel_changed = bef.channel != after.channel
+        LOGGER.debug("voice state changed for user %s channel changed %s", mem.name, channel_changed)
         if after.channel is None:
+            LOGGER.debug("user %s left channel", mem.name)
             return
         if after.channel.id in self._timeouts:
+            LOGGER.debug("channel %s is still timed out", after.channel.name)
             return
         if channel_changed and len(after.channel.members) == 1:
+            LOGGER.info("%s just joined channel %s(%a)", mem.name, after.channel.name, after.channel.id)
+            LOGGER.info("channel members %a", after.channel.members)
             self._timeouts.add(after.channel.id)
             for user in self._subs[after.channel.id]:
+                LOGGER.info("notifying user %s", user.name)
                 if user != mem:
                     await user.send(f"Activity started on {after.channel} by {mem.name}")
             asyncio.create_task(self._revoke_timeout(after.channel.id))
 
     async def _revoke_timeout(self, channel_id):
         await asyncio.sleep(PER_CHAN_TIMEOUT)
+        LOGGER.debug("revoking timeout on channel %A", channel_id)
         self._timeouts.remove(channel_id)
 
     async def _remove_context(self, user_id):
         await asyncio.sleep(USER_CTX_TIMEOUT)
+        LOGGER.debug("removing user %a context", user_id)
         del self._users_context[user_id]
 
 def run_client(token):
